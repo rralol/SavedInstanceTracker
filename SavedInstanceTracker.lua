6061d502b0afaca4ccc94a604a2aea11dd0441fa
rralol <56774516+rralol@users.noreply.github.com>
2019-10-25 15:00:32 +0200

    Minor changes

--- SavedInstanceTracker.lua
+++ SavedInstanceTracker.lua
@@ -1,5 +1,5 @@
 --Variables for communication
-iComm = LibStub("AceAddon-3.0"):NewAddon("SavIT", "AceComm-3.0")
+iComm = LibStub("AceAddon-3.0"):NewAddon("SavedIn", "AceComm-3.0")
 
 local LibAceSerializer = LibStub:GetLibrary("AceSerializer-3.0")
 local LibCompress = LibStub:GetLibrary("LibCompress")
@@ -19,7 +19,7 @@ local delayedReset = false;
 local readyTicker;
 
 --Custom print function for addon messages.
-function savedInPrint(...)
+function SavedInPrint(...)
 	local savedIn = "|cFFFF6464SavedIn|r:";
 	DEFAULT_CHAT_FRAME:AddMessage(string.join(" ", savedIn, ...));
 end
@@ -29,29 +29,29 @@ function iComm:OnCommReceived(prefix, message, distribution, sender)
 	if prefix == "SavedIn" then
 		if message == "ResetBroadcast" then
 			if not UnitIsGroupLeader("player") and UnitIsGroupLeader(sender) then
-				savedInPrint("Recieved reset message from party leader.");
+				SavedInPrint("Recieved reset message from party leader.");
 				allowMultipleOfInstance = true;
 			end
 		elseif message  == "ResetRequest" then
 			if UnitIsGroupLeader("player") and allowResetRequest then
-				savedInPrint("Recieved reset reqeust from party member sending confirmation.");
+				SavedInPrint("Recieved reset reqeust from party member sending confirmation.");
 				iComm:sendMessage("RequestConfirm");
 				--Start the timer for checking if all player are ready for reset run for 120 seconds
-				readyTicker = C_Timer.NewTicker(1, function () checkPlayersResetReady(); end, 120)
+				readyTicker = C_Timer.NewTicker(1, function () CheckPlayersResetReady(); end, 120)
 			end
 		elseif message == "RequestConfirm" then
 			local inInstance,_ = IsInInstance();
 			--allowMultipleOfInstance = true
 			if inInstance and not UnitIsGroupLeader("player") then
-				savedInPrint("Recieved reset confirmation from party leader leave the instance or logout.");
+				SavedInPrint("Recieved reset confirmation from party leader you have 2 minutes to leave the instance or logout.");
 			elseif not UnitIsGroupLeader("player") then
-				savedInPrint("Recieved reset confirmation from party leader please stay out of the instance.");
+				SavedInPrint("Recieved reset confirmation from party leader please stay out of the instance.");
 			end
 		end
 	end
 end
 
-function checkPlayersResetReady()
+function CheckPlayersResetReady()
 	--Check if all players in party is offline or outside instance
 	local partyMembers = GetHomePartyInfo();
 	local inInstance,_ = IsInInstance();
@@ -66,7 +66,7 @@ function checkPlayersResetReady()
 		end
 		if allReady then
 			readyTicker:Cancel();
-			savedInPrint("All players are ready for reset.");
+			SavedInPrint("All players are ready for reset.");
 			ResetInstances();
 		end
 	end
@@ -78,12 +78,12 @@ function iComm:sendMessage(message)
 end
 
 --Broadcast reset to other pary members
-local function BroadcastResetDungeon()	
+local function BroadcastResetDungeonSuccess()	
 	if IsInGroup() and UnitIsGroupLeader("player") then
-		savedInPrint("Resetting dungeons. Sending reset command to other party memebers.")
+		SavedInPrint("Sending reset successful to other party memebers.")
 		iComm:sendMessage("ResetBroadcast");
 	else
-		savedInPrint("Resetting dungeons.")
+		SavedInPrint("Resetting dungeons.")
 	end
 end
 
@@ -107,7 +107,7 @@ local function PurgeExpiredEntries()
 end
 
 --Checks the differens in time between timestamp and current time and returns result minutes
-local function getTimeToReset(index)
+local function GetTimeToReset(index)
 	if SavedInstances_DB[index].timeEnd > 0 then
 		local timeToReset = SavedInstances_DB[index].timeEnd - time()
 		return math.ceil(timeToReset/60)
@@ -122,13 +122,13 @@ local function HandleSlashCommands(str)
 	if #str == 0 then 
 		-- Print the DBTable
 		if #SavedInstances_DB == 0 then
-			savedInPrint("No active lockouts.");
+			SavedInPrint("No active lockouts.");
 		else 
 			for i=1, #SavedInstances_DB do
-				if getTimeToReset(i) > 0 then
-					savedInPrint(i..": "..SavedInstances_DB[i].instanceName.." |cFFFF6464Releases in|r : "..getTimeToReset(i).." minutes");
+				if GetTimeToReset(i) > 0 then
+					SavedInPrint(i..": "..SavedInstances_DB[i].instanceName.." |cFFFF6464Releases in|r : "..GetTimeToReset(i).." minutes");
 				else
-					savedInPrint(i..": "..SavedInstances_DB[i].instanceName.." |cFFFF6464Dungeon in progress|r!");
+					SavedInPrint(i..": "..SavedInstances_DB[i].instanceName.." |cFFFF6464Dungeon in progress|r!");
 				end
 			
 			end
@@ -136,15 +136,15 @@ local function HandleSlashCommands(str)
 	--Request reset from leader
 	elseif str == "request" then
 		if IsInGroup() then
-			savedInPrint("Sending reset request to leader.")
+			SavedInPrint("Sending reset request to leader.")
 			iComm:sendMessage("ResetRequest");
 		else 
-			savedInPrint("Reseting dungeon when outside of dungeon.")
+			SavedInPrint("Reseting dungeon when player exits dungeon.")
 			local inInstance,_ = IsInInstance();
 			if inInstance then
 				delayedReset = true;
 			else
-				savedInPrint("Player outside of instance reseting dungeon!");
+				SavedInPrint("Player outside of instance reseting dungeon!");
 				C_Timer.After(1, ResetInstances);
 			end
 		end
@@ -152,45 +152,45 @@ local function HandleSlashCommands(str)
 	elseif str == "reset" then
 		local inInstance,_ = IsInInstance();
 		if inInstance then
-			savedInPrint("Forcing new instance.")
+			SavedInPrint("Forcing new instance lockout to be added.")
 			local instanceName,_,_,_,_,_,_,instanceId,_ = GetInstanceInfo();
 			SavedInstances_DB[#SavedInstances_DB].timeEnd = time() + 3600;
 			SavedInstances_DB[#SavedInstances_DB].active = false;
-			addInstanceLockout(instanceName, instanceId);
+			AddInstanceLockout(instanceName, instanceId);
 		else
-			savedInPrint("Next instance will count as a new instance.")
+			SavedInPrint("Next instance will count as a new instance.")
 			allowMultipleOfInstance = true;
 		end
 	--Run soft reset command
 	elseif str == "softreset" then 
 		allowMultipleOfInstance = true;
-		savedInPrint("Next instance will count as a new instance.")
+		SavedInPrint("Next instance will count as a new instance.")
 	elseif str == "allowRequest" then
 		if allowResetRequest then
-			savedInPrint("Disallowing party reset requests.")
+			SavedInPrint("Disallowing party reset requests.")
 		else
-			savedInPrint("Allowing party reset requests.")
+			SavedInPrint("Allowing party reset requests.")
 		end
 	--Print list of commands
 	elseif str == "?" or "help" then
-		savedInPrint("List of commands:")
-		savedInPrint("/sav reset - Forces a new entry of the current instance.")
-		savedInPrint("/sav softreset - Tells the addon to expect a fresh instance next time one of the same name is entered.")
-		savedInPrint("/sav request - Request an automatic reset from the leader when possible.")
-		savedInPrint("/sav allowRequest - Toggles between allowing and disallowing party members to request resets.")
-		savedInPrint("/sav help - Display a list and description of available commands.") 
+		SavedInPrint("List of commands:")
+		SavedInPrint("/sav reset - Forces a new entry of the current instance.")
+		SavedInPrint("/sav softreset - Tells the addon to expect a fresh instance next time one of the same name is entered.")
+		SavedInPrint("/sav request - Request an automatic reset from party leader(requires leader to SavedInstanceTracker addon).")
+		SavedInPrint("/sav allowRequest - Toggles between allowing and disallowing party members to send request resets when party leader.")
+		SavedInPrint("/sav help - Display a list and description of available commands.") 
 	end
 end
 
 
-local function addInstanceLockout(name, id)
-	savedInPrint("New instance detected, adding to table.");
+local function AddInstanceLockout(name, id)
+	SavedInPrint("New instance detected, adding to table.");
 	allowMultipleOfInstance = false;
 	tinsert(SavedInstances_DB,{instanceName = name, countdownActive = false, mapid = id, timeEnd = 0, backupEndTime = time() + 3600 , player = select(1 ,UnitName("player"))})
 end
 
 --Check if an instance of the same name tied to the same character already exists in the DB
-local function checkMultipleEntries(id)
+local function CheckMultipleEntries(id)
 	for i=1, #SavedInstances_DB do
 		if SavedInstances_DB[i].mapid == id and SavedInstances_DB[i].player == select(1 ,UnitName("player")) then
 			return true
@@ -212,7 +212,7 @@ local function OnInitialize(event, name)
 	if allowResetRequest == nil then allowResetRequest = true end;
 end
 
-local function getCurrentLocation()
+local function GetCurrentLocation()
 	local name, instanceType, difficultyID, difficultyName, maxPlayers, dynamicDifficulty, isDynamic, instanceID, instanceGroupSize, LfgDungeonID = GetInstanceInfo();
 	local location = {};
 	if instanceType == "party" or instanceType == "raid" then
@@ -225,23 +225,23 @@ local function getCurrentLocation()
 	return location;
 end
 
-local function updateCurrentLocation()
-	currentLocation = getCurrentLocation();
+local function UpdateCurrentLocation()
+	currentLocation = GetCurrentLocation();
 end
 
 --Checking for offline reset
 local function checkForOfflineReset()
 	--Debug
-	--savedInPrint("Curzone: "..currentLocation.subZone.. " Lastzone: "..lastLocation.subZone);
+	--SavedInPrint("Curzone: "..currentLocation.subZone.. " Lastzone: "..lastLocation.subZone);
 	if currentLocation.instanceId == lastLocation.instanceId and currentLocation.subZone ~= lastLocation.subZone then
-		savedInPrint("true");
+		SavedInPrint("true");
 		return true;
 	else
 		return false;
 	end
 end
 
-local function updateGroupStatus()
+local function UpdateGroupStatus()
 	if IsInGroup() then
 		wasInGroup = true;
 	else
@@ -249,40 +249,40 @@ local function updateGroupStatus()
 	end
 end
 
-local function eventHandler(self, event, arg1, ...)
+local function EventHandler(self, event, arg1, ...)
 	if event == "ADDON_LOADED" then
 		OnInitialize(event, arg1);
 		self:UnregisterEvent("ADDON_LOADED")
 		
 	elseif event == "PLAYER_ENTERING_WORLD" then
 		--Update session variables
-		updateCurrentLocation();
-		updateGroupStatus();
+		UpdateCurrentLocation();
+		UpdateGroupStatus();
 		--Check if in raid or dungeon
 		local inInstance,instanceType = IsInInstance();
 		if inInstance then
 		
 			local instanceName,_,_,_,instanceMaxPlayers,_,_,instanceId,_ = GetInstanceInfo();
 			--Check if an active instance of the same name exists 
-			if checkMultipleEntries(instanceId) and allowMultipleOfInstance == false then
+			if CheckMultipleEntries(instanceId) and allowMultipleOfInstance == false then
 				--Check for offline reset
 				if checkForOfflineReset() then
 					--Set endtime for last instance
-					savedInPrint("Offline reset detected.");
+					SavedInPrint("Offline reset detected.");
 					SavedInstances_DB[#SavedInstances_DB].timeEnd = time() + 3600;
-					addInstanceLockout(instanceName, instanceId);
+					AddInstanceLockout(instanceName, instanceId);
 				else
 					-- Set endTime to 0 since the instance is still active
-					savedInPrint("Player reentered active lockout reseting countdown.");
+					SavedInPrint("Player reentered active lockout reseting countdown.");
 					SavedInstances_DB[#SavedInstances_DB].timeEnd = 0
 				end
 			else
-				addInstanceLockout(instanceName, instanceId);
+				AddInstanceLockout(instanceName, instanceId);
 			end
 		-- Run once everytime a player leaves instance
 		elseif not inInstance and #SavedInstances_DB > 0 then
 			if SavedInstances_DB[#SavedInstances_DB].countdownActive == false then
-				savedInPrint("Player has left the dungeon starting lockout countdown.");
+				SavedInPrint("Player has left the dungeon starting lockout countdown.");
 				--Set endtime for last instance and set to inactive
 				SavedInstances_DB[#SavedInstances_DB].timeEnd = time() + 3600;
 				SavedInstances_DB[#SavedInstances_DB].countdownActive = true
@@ -291,44 +291,44 @@ local function eventHandler(self, event, arg1, ...)
 		
 		--Check for delayedReset if true resetdungeon(Used for Solo reset request)
 		if delayedReset and not inInstance and not IsInGroup() then
-			savedInPrint("Player outside of instance reseting dungeon.");
+			SavedInPrint("Player outside of instance reseting dungeon.");
 			C_Timer.After(1, ResetInstances);
 			delayedReset = false;
 		end
 		
 	elseif event == "PLAYER_LOGIN" then
 		PurgeExpiredEntries();
-		savedInPrint("Addon version "..version.." loaded. You currently have "..#SavedInstances_DB.."/5 lockouts active!");
-		savedInPrint("Use /sav help - for more information!");
+		SavedInPrint("Addon version "..version.." loaded. You currently have "..#SavedInstances_DB.."/5 lockouts active!");
+		SavedInPrint("Use /sav help - for more information!");
 		
 	elseif event == "ZONE_CHANGED_NEW_AREA" then
 		local inInstance,_ = IsInInstance();
 		if inInstance then
 			--Debug
-			--savedInPrint(GetSubZoneText());
+			--SavedInPrint(GetSubZoneText());
 			--Update location
-			updateCurrentLocation();
+			UpdateCurrentLocation();
 		end
 	elseif event == "PLAYER_CAMPING" then
 		--Set logout location
-		lastLocation = getCurrentLocation();
+		lastLocation = GetCurrentLocation();
 	elseif event == "GROUP_ROSTER_UPDATE" then
 		--Reset if leaving group outside of instance.
 		local inInstance,_ = IsInInstance();
 		if not inInstance and not IsInGroup() and wasInGroup then
 			--debug
-			--savedInPrint("Player leaving party outside of instance.");
+			--SavedInPrint("Player leaving party outside of instance.");
 			allowMultipleOfInstance = true
 		end
 	elseif event == "INSTANCE_BOOT_START" then
 		--Reset instance
 		allowMultipleOfInstance = true
-		updateGroupStatus();
+		UpdateGroupStatus();
 		
 	elseif event == "INSTANCE_BOOT_STOP" then
 		if IsInGroup() then 
 			allowMultipleOfInstance = false
-			updateGroupStatus();
+			UpdateGroupStatus();
 		end
 	-- Detect Dungeon Reset through chat message
 	elseif event == "CHAT_MSG_SYSTEM" then
@@ -336,7 +336,7 @@ local function eventHandler(self, event, arg1, ...)
 		--Check for reset message and broadcast if found
 		if arg1:match(resetMessagePattern) then
 			allowMultipleOfInstance = true;
-			BroadcastResetDungeon();
+			BroadcastResetDungeonSuccess();
 		end
 	end
 end
@@ -350,8 +350,6 @@ events:RegisterEvent("PLAYER_LOGIN");
 events:RegisterEvent("PLAYER_CAMPING");
 events:RegisterEvent("PLAYER_ENTERING_WORLD");
 events:RegisterEvent("GROUP_ROSTER_UPDATE");
-events:RegisterEvent("ZONE_CHANGED");
-events:RegisterEvent("ZONE_CHANGED_INDOORS");
 events:RegisterEvent("ZONE_CHANGED_NEW_AREA");
 events:RegisterEvent("CHAT_MSG_SYSTEM");
-events:SetScript("OnEvent", eventHandler);
+events:SetScript("OnEvent", EventHandler);
